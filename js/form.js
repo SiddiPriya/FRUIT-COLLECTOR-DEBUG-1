d44348c273c18f70a249cd704fd2cec820b9c632
SiddiPriya <75246300+SiddiPriya@users.noreply.github.com>
2021-04-20 11:35:29 +0530

    Update form.js

--- js/form.js
+++ js/form.js
@@ -14,18 +14,18 @@ class Form{
     }
     display() {
         this.title.html("FRUIT CATCHER");
-        this.title.position(2000, 50);
+        this.title.position(500,50);
         this.title.style('font-size', '70px');
         this.title.style('color', 'red');
-        this.input.position(2000,400);
+        this.input.position(500,400);
         this.input.style('width', '200px');
         this.input.style('height', '20px');
         this.input.style('background', 'lavender');
-        this.button.position(2000,500);
+        this.button.position(500,500);
         this.button.style('width', '200px');
         this.button.style('height', '40px');
         this.button.style('background', 'green');
-        this.reset.position(2050, 660);
+        this.reset.position(500, 660);
         this.reset.style('width', '100px');
         this.reset.style('height', '30px');
         this.reset.style('background', 'blue');
